1a09fb1f0b0d905f1a6f2386f976b78b890ab220
Vlad <vlad.zhuro.20@gmail.com>
2022-02-26 00:04:11 +0300

    added controllers

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,15 @@
+class DeviceController {
+    async create(req, res) {
+
+    }
+    async getOne(req, res) {
+
+    }
+
+    async getAll(req, res) {
+
+    }
+
+}
+
+module.exports = DeviceController();
---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+const Router = require('express');
+const brandRouter = require('./brandRouter');
+const deviceRouter = require('./deviceRouter');
+const typeRouter = require('./typeRouter');
+const userRouter = require('./userRouter');
+const errorHandler = require('../middleware/ErrorHandleMiddleware');
+
+const router = new Router();
+
+
+router.use('/user', userRouter);
+router.use('/type', typeRouter);
+router.use('/brand', brandRouter);
+router.use('/device', deviceRouter);
+
+module.exports = router;
